9f250f1ee3f7ffcbaa0313f8b9587ee1af560966
Aaronicss <lazaroaaron329@gmail.com>
2026-05-31 23:06:22 +0800

    imports amirite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import NavBar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
